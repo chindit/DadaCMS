1dc3fb92b6ef81933d1688a6bbb0dbf8c39b93dd
chindit <littletiger58@gmail.com>
2016-06-01 09:16:07 +0200

    Fix: initial config

--- DependencyInjection/DadaCMSExtension.php
+++ DependencyInjection/DadaCMSExtension.php
@@ -21,6 +21,10 @@ class DadaCMSExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+        
+        $container->setParameter('dadacms.history', $config['dadacms.history']);
+        $container->setParameter('dadacms.items_page', $config['dadacms.items_page']);
+        $container->setParameter('dadacms.default_role', $config['dadacms.default_role']);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
